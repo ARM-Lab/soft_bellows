ac8bbaa849cf309cef98fb106642c261279cf42e
ivanvirgala <ivan.virgala@tuke.sk>
2024-05-17 15:40:46 +0200

    plc_interface

---
+++ soft_bellows_plc_interface/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'soft_bellows_plc_interface'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='km',
+    maintainer_email='ivan.virgala@tuke.sk',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "plc_interface = soft_bellows_plc_interface.plc_interface:main"
+        ],
+    },
+)
